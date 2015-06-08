00338cd40e59a3d7a50ddee25355766cdc718c29
Jeff Wolf <jeffcwolf@gmail.com>
2015-06-08 09:14:25 -0500

    Modifies user interface in driver file

--- main.rb
+++ main.rb
@@ -12,19 +12,40 @@ user.load_data
 
 puts "\nData has been loaded. Choose your search criteria below."
 
-puts "\n#{user_name.upcase}'s options:"
-puts "(1) Type '1' to search by site name. [Type '1_list' to print a list of ALL site names (#{@xml_names.uniq.size} total)]"
-puts "(2) Type '2' to search by country name. [Type '2_list' to print a list of ALL country names (#{@xml_states.uniq.size} total)]"
-puts "(3) Type '3' to search by region name. [Type '3_list' to print a list of ALL region names (#{@xml_regions.uniq.size} total)]"
-puts "(4) Type '4' to search by site category. [Type '4_list' to print a list of ALL category names (#{@xml_categories.uniq.size} total)"
-puts "(5) Type '5' to search the text of site descriptions"
-puts "(6) Type '6' to find a random site"
-puts "(7) Type '7' to generate a random itinerary (a collection of sites) of a chosen size (e.g. 10 random sites)"
-puts "(8) Type '8' to perform an advanced, multi-criteria search (e.g. find all cultural sites in Germany)"
+loop do
+  puts "\n#{user_name.upcase}'s options (type 'quit' to exit):"
+  puts "(1) Type '1' to search by site name. [Type '1_list' to print a list of ALL site names (#{@xml_names.uniq.size} total)]"
+  puts "(2) Type '2' to search by country name. [Type '2_list' to print a list of ALL country names (#{@xml_states.uniq.size} total)]"
+  puts "(3) Type '3' to search by region name. [Type '3_list' to print a list of ALL region names (#{@xml_regions.uniq.size} total)]"
+  puts "(4) Type '4' to search by site category. [Type '4_list' to print a list of ALL category names (#{@xml_categories.uniq.size} total)"
+  puts "(5) Type '5' to search the text of site descriptions"
+  puts "(6) Type '6' to find a random site"
+  puts "(7) Type '7' to generate a random itinerary (a collection of sites) of a chosen size (e.g. 10 random sites)"
+  puts "(8) Type '8' to perform an advanced, multi-criteria search (e.g. find all cultural sites in Germany)"
 
-answer = gets.chomp
+  answer = gets.chomp.downcase
 
-# case answer
+  case answer
+  # when [include regex here]
 
-# when
+  # Process print list commands
+  when '1_list'
+    puts @xml_names.uniq.sort
+  when '2_list'
+    puts @xml_states.uniq.sort
+  when '3_list'
+    puts @xml_regions.uniq.sort
+  when '4_list'
+    puts @xml_categories.uniq.sort
 
+  # Process basic search commands
+
+
+  #Process quit command
+  when 'quit', 'q', 'exit'
+    puts "See you again soon!"
+    break
+  else "Please enter an option number or type 'quit' to exit."
+  end
+
+end
